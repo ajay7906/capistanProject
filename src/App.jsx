b6aea5744afae335894b587c68c39fae35f06347
Ajay Kumar <aks969014@gmail.com>
2024-01-27 18:21:54 +0530

    Boilerplate React setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import RegistrationPage from './componenets/RegisterForm'
+
+function App() {
+ 
+  return (
+    <>
+     <RegistrationPage/>
+    </>
+  )
+}
+
+export default App
